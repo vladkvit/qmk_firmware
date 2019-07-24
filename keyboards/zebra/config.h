0d32e92567ebca632e68263badc15715efabf338
Vlad Kvitnevskiy <vladkvit@outlook.com>
2019-07-24 08:02:06 -0700

    Init commit, Zebra keyboard. Compiles, but keymaps are off, LED driver is off, split is off.

---
+++ keyboards/zebra/config.h
@@ -0,0 +1,71 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xCB10
+#define PRODUCT_ID      0x1169
+#define DEVICE_VER      0x0100
+#define MANUFACTURER    vladkvit
+#define PRODUCT         Zebra
+#define DESCRIPTION     Split ortho ergo keyboard
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 8
+
+// wiring of each half
+#define MATRIX_ROW_PINS { B2, B3, B1, F7, F6, F5, F4 }
+#define MATRIX_COL_PINS { B5, B4, D7, C6, D4, D2, D3 }
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* serial.c configuration for split keyboard */
+#define SOFT_SERIAL_PIN E6
+
+/* ws2812 RGB LED */
+// #define RGB_DI_PIN B4
+// #define RGBLED_NUM 20    // Number of LEDs
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
